4a153b6cf74e16d9cecfb152d7d86c921a7802ae
gcid <gcid12@gmail.com>
2014-08-05 10:45:53 -0400

    Details Box
    Added: Battletag, co-founding and Work in DB, update  and Members.

--- application/views/pages/member.php
+++ application/views/pages/member.php
@@ -15,28 +15,29 @@
   </div>
   <div class="col-xs-7 col-md-7 text-left">
 
-   	<div class="pull-right" style="color:#3A3A3A;">
+   	<div class="pull-right" style="color:#666;">
   		<h2>23 
   			<i class="fa fa-bolt fa-2x"></i>
   		</h2>
   	</div>
 
-  <h1 class="lead" style="font-size:3em;"><?php echo $tcbuser->first_name." ".$tcbuser->last_name; ?></h1>
+
+  <h1 class="lead" style="font-size:3em;"><?php echo $user->first_name." ".$user->last_name; ?></h1>
   	
 		
 
   		<?php
-  		$k01=$tcbuser->k01;
-  		$k02=$tcbuser->k02;
-  		$k03=$tcbuser->k03;
-  		$k04=$tcbuser->k04;
-  		$k05=$tcbuser->k05;
+  		$k01=$user->k01;
+  		$k02=$user->k02;
+  		$k03=$user->k03;
+  		$k04=$user->k04;
+  		$k05=$user->k05;
 
-  		$s01=$tcbuser->s01;
-  		$s02=$tcbuser->s02;
-  		$s03=$tcbuser->s03;
-  		$s04=$tcbuser->s04;
-  		$s05=$tcbuser->s05;
+  		$s01=$user->s01;
+  		$s02=$user->s02;
+  		$s03=$user->s03;
+  		$s04=$user->s04;
+  		$s05=$user->s05;
 
   		?>
 
@@ -60,21 +61,21 @@
 
 
 				<div class="lead">
-				<?php echo $tcbuser->city; ?> , 
-				<?php echo $tcbuser->country; ?> |
+				<?php echo $user->city; ?> , 
+				<?php echo $user->country; ?> |
 				
 
 				<?php 
-					if($tcbuser->showemail==1){
-				echo $tcbuser->email." | "; 
+					if($user->showemail==1){
+				echo $user->email." | "; 
 							}
 				?> 
 
 
-				<strong>@</strong>  <?php echo $tcbuser->s01; ?> 
+				<strong>@</strong>  <?php echo $user->s01; ?> 
 				</div>
 
-				<div><?php echo $tcbuser->pitch; ?></div>
+				<div><?php echo $user->pitch; ?></div>
 				<br/><br/>
 
 
@@ -94,34 +95,107 @@
 
 			<div class=" col-sm-6 text-left basetxt txtsmall graytxt1">
 					<h3 class="lead text-muted">About me</h3>
-					<div><?php echo $tcbuser->about; ?></div>
+					<div><?php echo $user->about; ?></div>
 
 					<h3 class="lead text-muted">Looking for </h3>
-					<div><?php echo $tcbuser->iwant; ?></div>
-
-					<hr/>
-					<h3 class="lead text-muted"> </h3>
-
-					
-
-					<?php //echo $tcbuser->first_name; ?> 
+					<div><?php echo $user->iwant; ?></div>
+					<br/><br/>
 
 
 							<?php 
-							// $pm01=$tcbuser->pm01;
-							// $pm02=$tcbuser->pm02;
-							// $pm03=$tcbuser->pm03;
+							 $pm01=$user->pm01;
+							 $pm02=$user->pm02;
+							 $pm03=$user->pm03;
 
-							// $pay01=$tcbuser->pay01;
-							// $pay02=$tcbuser->pay02;
-							// $pay03=$tcbuser->pay03;
+							 $pay01=$user->pay01;
+							 $pay02=$user->pay02;
+							 $pay03=$user->pay03;
 
 							?>
 
-					<?php //if (isset($pm01)) { $this->tcb_functions->tcb_payment_method($pm01,$pay01); } ?>
-					<?php //if (isset($pm02)) { $this->tcb_functions->tcb_payment_method($pm02,$pay02); } ?>
-					<?php //if (isset($pm03)) { $this->tcb_functions->tcb_payment_method($pm03,$pay03); } ?>
+				<div class="well text-muted" style="background-color:#000;">
 
+					<div class="lead">Details</div>
+
+					
+
+					<!-- Work: <i class="fa fa-check tcb-color"></i> Remote | 
+					<i class="fa fa-check tcb-color"></i> On Site<br/> -->
+
+					<!-- Languages: <i class="fa fa-language tcb-color"></i> ENG, SPA, FRA<br/> -->
+
+
+				<?php  //WORK
+						$work=$user->work;
+							if(isset($work)){
+								echo "Work:";
+								if($work=='rm'){ $icon="fa-gear"; $message="Remote";}
+								elseif($work=='os'){$icon="fa-gear"; $message="On-Site";} 
+								elseif($work=='fl'){$icon="fa-gear"; $message="Flexible";} 
+								else{$icon="fa-gear"; $message="Contact me";}
+								echo " &nbsp;<i class='fa ".$icon." tcb-color'></i>  <span class='tcb-color'> ".$message."</span><br/>";
+							}	
+					?>
+
+					<?php  //payment preferences
+							 $pm01=$user->pm01;
+							 $pm02=$user->pm02;
+							 $pm03=$user->pm03;
+
+							 $pay01=$user->pay01;
+							 $pay02=$user->pay02;
+							 $pay03=$user->pay03;
+
+					if(isset($pm01) || isset($pm02) || isset($pm03)) echo "Payments preferences: <br/>";
+
+					if (isset($pm01)) { $this->tcb_functions->tcb_payment_method($pm01,$pay01); } 
+					if (isset($pm02)) { $this->tcb_functions->tcb_payment_method($pm02,$pay02); } 
+					if (isset($pm03)) { $this->tcb_functions->tcb_payment_method($pm03,$pay03); } 
+					?>
+
+					<br/>
+
+
+					<?php  //COFOUND
+						$cofound=$user->cofound;
+							if(isset($cofound) || $cofound!="no"){
+								
+								if($cofound=='ye'){ $message="yes";}
+								elseif($cofound=='wc'){$message="Will consider";} 
+								elseif($cofound=='me'){$message="As Mentor";}
+								elseif($cofound=='cm'){$message="Contact Me";}
+								else{$message="no";} 
+
+								if($cofound=='no'){ //silence 
+								}else{echo "Co-founding: ".$message."<br/>";} 
+								
+							}	
+					?>
+
+					<?php //RECRUITERS
+						if($user->recru==1){ 
+							echo "Recruiters: <i class='fa fa-check tcb-color'></i> Its ok to contact me. <br/>";
+						}else{ echo "Recruiters: I rather not be contacted, thanks. <br/>";}
+						?>
+
+					<?php  //TSHIRT
+						$gender=$user->gender;
+						$tshirt=$user->tshirt;
+							if(isset($tshirt)){
+								echo "Tshirt Size:";
+								if($gender=='m'){ $icon="fa-male";}elseif($gender=='f'){$icon="fa-female";} 
+								echo " &nbsp;<i class='fa ".$icon." tcb-color'></i>  <span class='tcb-color'> ".$tshirt."</span><br/>";
+							}	
+					?>
+
+					<?php  //BATTLETAG
+						$battletag=$user->battletag;
+							if(isset($battletag)){
+								echo "Battletag: <i class='fa fa-rocket tcb-color'></i> ".$battletag."<br/>";	
+							}	
+					?>
+
+				</div>
 		  </div>
 
 			<div class="col-sm-6 text-left basetxt txtsmall graytxt1">
@@ -129,12 +203,25 @@
 					<br/><br/>
 
 					<?php if (isset($s01)) {?>
-						<a data-screen-name="<?php echo "$s01";?>" class="twitter-timeline" href="https://twitter.com/<?php echo "$s01";?>" data-widget-id="472368192304586752"><?php echo "$s01";?></a>
-						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+					<a data-screen-name="<?php echo "$s01";?>" class="twitter-timeline" href="https://twitter.com/techstars" data-widget-id="496444200594849793">Tweets by @techstars</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
+			
 					<?php }?>
 
 			</div>
 		</div> <!-- CLOSE ROW -->
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<hr/>
+
+
+
+			</div>
+
+		</div>
 
 
 

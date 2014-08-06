2634716bcdc3de18c1d43669ded83b92fb96883f
gcid <gcid12@gmail.com>
2014-08-06 19:48:23 -0400

    Everyone can invite
    If you are logged in, you can invite.

--- application/views/pages/topmenu.php
+++ application/views/pages/topmenu.php
@@ -1,7 +1,13 @@
 <div class="row">
   <div class="col-xs-12 ">
 
-	<span class="nametag"> 
+
+	
+
+
+			<?php if($logged==true){ ?>
+
+				<span class="nametag"> 
 						<?php  //name label
 						$first_name=$tcbuser->first_name; 
 						$gender=$tcbuser->gender;
@@ -10,19 +16,17 @@
 						<?php 
 						$this->tcb_functions->label_user($gender,$first_name); 
 						?>
-	</span>
-	
+				</span>		
+				<a href="/auth/logout" rel="tooltip" class="menulink wtt menu1" style="float:right;" title="SIGN OUT"><i class="fa fa-sign-out fa-2x"></i></a>
+				<a href="/tcb/backstage/<?php echo $tcbuser->id;?>" rel="tooltip" class="menulink wtt menu1" style="float:right;" title="BACKSTAGE"><i class="fa fa-plus fa-2x"></i></a> 
 
 
-			<?php if($logged==false){ ?>
-				<a href="/auth/login" rel="tooltip" class="menulink wtt menu1" style="float:right;" title="SIGN IN"><i class="fa fa-sign-in fa-2x"></i></a>
+				
+				
+			<?php }else{ ?>	
+
 				Logged OUT
-			<?php }else{ ?>			
-
-						<a href="/auth/logout" rel="tooltip" class="menulink wtt menu1" style="float:right;" title="SIGN OUT"><i class="fa fa-sign-out fa-2x"></i></a>
-
-						<a href="/tcb/backstage/<?php echo $tcbuser->id;?>" rel="tooltip" class="menulink wtt menu1" style="float:right;" title="BACKSTAGE"><i class="fa fa-plus fa-2x"></i></a> 
-
+				<a href="/auth/login" rel="tooltip" class="menulink wtt menu1" style="float:right;" title="SIGN IN"><i class="fa fa-sign-in fa-2x"></i></a>
 						
 		  			
 				<?php } ?>

19f248b6290607dda380a98b732e47f8c10d3cc8
gcid <gcid12@gmail.com>
2014-08-06 14:58:21 -0400

    Update in About TCB
    TCB and About are merged in just one section

--- application/views/pages/story.php
+++ application/views/pages/story.php
@@ -14,23 +14,149 @@
     <img class="img-responsive" src="<?php echo base_url('assets/img/tcb/846766.gif')."?=".rand(1,100); ?>" alt="846766"/>
   </div>
   <div class="col-xs-7 col-md-7 text-left">
-      <span class="txttitle" title="UX Designer">The original T.C.B.</span>
+      <h1 class="lead" style="font-size:3em;">About TCB 846766</h1>
       <br/>
-      <br/>
-    
-        <div class="fhr"></div>   
-          <p title="UX Designer" class="txtsmall">
- 
+
+
+          <div class="row">
+            <div class=" col-sm-12 text-left basetxt txtsmaller graytxt1">
+                <div class="lead" style="font-size:3em;">TCB it's a network of Tech Talent looking for projects to increase their experience, get exposure and build their network.
+                </div> 
+
+            </div>
+          </div> 
+
+          <!-- <div class="row">
+            <div class=" col-sm-6 txtsmaller graytxt1">
+              <p class="lead">As a founder, owner</p>
+              Have access to active players, that you normally won't have access to because they are currently employed by other company. Hire a Growth Hacker specialist for 2 days, a financial expert or a specialist in DB to take a look at your problem from a fresh point of view.
+            </div>
+            <div class=" col-sm-6 txtsmaller graytxt1">
+              <p class="lead">As a skilled Tech</p>
+              Meet new people, get out of a routine, beef up your CV and skills with different set of problems.
+            </div>
+          </div>   -->
+
+
+        <div class="fhr3"></div> 
+
+          <div class="row">
+            <div class=" col-sm-8 text-left basetxt txtsmaller graytxt1">
+                <div class="lead">Who's behind TCB?
+                </div> 
+                <p class="txtsmall">
+                TCB was founded by former Hackstars and Associates that went through the Techstars program, we are sworn by the "Do more Faster" mantra. We iterate fast, we get things done. However TCB is not exclusive to former Techstars , we are always looking for amazing people to join us.
+                </p>
+            </div>
+          
+            <div class=" col-sm-4 text-left basetxt txtsmaller graytxt1">
+                <img class="img-responsive" src="<?php echo base_url('assets/img/tcb/tcb_badge2.png'); ?>" alt="846766"/>
+            </div>
+          </div> 
+
+          <div class="fhr3"></div> 
+
+          <div class="row">
+            <div class=" col-sm-7 text-left basetxt txtsmall graytxt1">
+                <br/>
+                <div class="lead">So what the heck is 846766 ???
+                </div> 
+            </div>
+          
+            <div class=" col-sm-5 text-left basetxt txtsmaller graytxt1 text-center">
+                
+                <br/>
+                <code>
+                &#38;&#35;&#56;&#52;&#59; 
+                &#38;&#35;&#54;&#55;&#59; 
+                &#38;&#35;&#54;&#54;&#59;
+                </code>
+
+                <br/><br/>
+                <p style="font-size:0.7em;">84, 67 and 66 are the ASCII characters codes <br/>for the letters T,C and B <p/>
+
+            </div>
+          </div> 
+
+            <div class="fhr3"></div> 
+          <div class="row">
+            <div class=" col-sm-4 text-left basetxt graytxt1">
+                <img class="img-responsive" src="<?php echo base_url('assets/img/tcb/tcb_stickerpack.png'); ?>" alt="846766"/>
+            </div>
+            <div class=" col-sm-8 text-left basetxt txtsmaller graytxt1">
+                <div class="lead">Stickers
+                </div> 
+                We provide unlimited stickers supply* for TCB members. It might take some time to reach everyone (and re-supply), but we are commited to make it happen. If you have a sticker that want to put in the hands of all the TCB network, contact us at: hello@tcb.io
+            </div>
+          </div> 
+
+          <div class="fhr3"></div>
+
+
           <div class="row">
 
-            <div class=" col-sm-7 text-left basetxt txtsmall graytxt1">
-      
-                The initials T.C.B. stand for Taking Care of Business, which is what Elvis Presley called his group of friends. He incorporated these letters into an emblem flanked by lightning bolts. This emblem was used in several pieces of Elvis' custom jewelry, including his famous T.C.B. ring, necklace, and his private aircraft. this was a group of friends, associates and employees whose main function was to just be around Elvis Presley from 1954 until he died. 
-      
-              <br/><br/>
-              </p> 
+            <div class=" col-sm-8 text-left basetxt txtsmaller graytxt1">
+                <div class="lead">TCB Blog
+                </div> 
+                Every TCB member is encouraged to post in the TCB Blog, This is the best way to establish some reputation and give opinion about anything related with their area of expertise. 
             </div>
-            <div class=" col-sm-5 basetxt">
+            <div class=" col-sm-4 text-left basetxt graytxt1">
+                <div style="padding:15px;"><img class="img-responsive" src="<?php echo base_url('assets/img/tcb/tcb_blogicon.png'); ?>" alt="846766"/></div>
+            </div>
+          </div> 
+          <div class="fhr3"></div> 
+
+
+          <div class="row">
+            <div class=" col-sm-4 text-left basetxt graytxt1">
+                <img class="img-responsive" src="<?php echo base_url('assets/img/tcb/tcb-key-sm.png'); ?>"/>
+            </div>
+            <div class=" col-sm-8 text-left basetxt txtsmaller graytxt1">
+                <div class="lead">TCB Early Adopters</div> 
+                
+                
+                Early adopters is a project where selected TCB Members receive acces to your service or a physical product . Then 2 things can happen, they fall in love with your product and become early adopters, or you'll receive back a bunch of constructive criticism that will help you refine your idea. 
+
+            </div>
+          </div> 
+
+          <div class="fhr3"></div>
+
+
+<!--  <ul>
+                    <li><div class="lead">TCB WALL</div> TCB members have access to the TCB wall, a forum where we have discussions about everything related to tech. </li>
+                    <li><div class="lead">TCB BLOG:</div> Every TCB member is encouraged to post in the TCB Blog, This is the best way to establish some reputation. The blog is mildly curated to keep some editorial consistency.</li>
+                    <li><div class="lead">TCB HACKATHONS:</div> Hackathons are the geek mingle event per excellence. Let's build relevant things.</li>
+                </ul>   -->  
+
+          
+
+          <div class="row">
+            <div class=" col-sm-8 text-left basetxt txtsmaller graytxt1">
+            <h2 class="lead">The original T.C.B.</h2>
+                <p>The initials T.C.B. stand for Taking Care of Business, 
+                which is what Elvis Presley called his group of friends. 
+                He incorporated these letters into an emblem flanked by lightning bolts. 
+                This emblem was used in several pieces of Elvis' custom jewelry, 
+                including his famous T.C.B. ring, necklace, and his private aircraft. 
+                This was a group of friends, associates and employees whose main function 
+                was to just be around Elvis Presley from 1954 until he died. 
+                </p>
+
+                  <div class="row">
+                    <div class="col-sm-8">
+                    </div>
+                    <div class="col-sm-4">
+                      <div class="fhr3"></div>
+                      <div class="text-muted basetxt" style="font-size:0.7em;">
+                          (1.)1968 Photo, Las Vegas NV, Some of the original members of TCB, (2.)A jewerly gift by Elvis to members of TCB,(3) Elvis Aircraft with TCB logo
+                      </div>
+                    </div>  
+                  </div>    
+
+              
+            </div>
+            <div class=" col-sm-4 basetxt">
         
               <a data-toggle="modal" data-target="#myModal1"> 
                 <img src="<?php echo base_url('assets/img/tcb/mem2.jpg') ?>" class="img-responsive" />
@@ -43,15 +169,32 @@
               <a data-toggle="modal" data-target="#myModal2">
                 <img src="<?php echo base_url('assets/img/tcb/tcbair.jpg') ?>" class="img-responsive" style="width:50%; float:left;"/>
               </a>
-              <div style="clear:both;"></div>
-        
-              <div class="fhr"></div>
-                <div class="text-muted basetxt" style="font-size:0.7em;">
-                  (1.)1968 Photo, Las Vegas NV, Some of the original members of TCB, (2.)A jewerly gift by Elvis to members of TCB,(3) Elvis Aircraft with TCB logo</div>
-                </div>
-              <div class="col-xs-1 col-md-1 text-center basetxt lead">
+            </div>
+            <div class="col-xs-1 col-md-1 text-center basetxt lead">
             </div>
           </div> <!-- CLOSE ROW -->
+
+
+          <div class="fhr3"></div> 
+
+          <div class="row">
+            <div class=" col-sm-6 text-left basetxt txtsmaller graytxt1">
+              <!-- <h2 class="lead">TCB is not:</h2>  
+                  <p>
+                    A recruiting agency/ headhunters. We do not ask or take comissions , the whole system is transparent so anyone can be contacted directly. If you are looking for a Full-time, permanent position maybe this is not the best place to start.
+                  </p> -->
+            </div>
+            <div class=" col-sm-6 text-left basetxt txtsmaller graytxt1">
+              <!-- <h2 class="lead">How do we do money</h2>  
+                  <p>
+                    <ol>
+                      <li>We do money by providing a "Legit Badges &#8482;" to members, this means that you can ask us to run a background check to you (References, skills,etc ). This is completely voluntary and optional, and can only be asked by the member. </li>
+                      <li> We allow Companies to spoil members with free stuff... not discount coupons, but useful stuff that TCB members can use. Every member have their Goodies Inbox. (Think Hunger Games meets hardware try-outs)</li>
+                      <li>We sell stickers and TCB846766 paraphernalia... but only for fun and to cover printing costs.</li>
+                  </p>  -->   
+ 
+            </div>
+          </div>  
 
 
         

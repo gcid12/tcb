61db831c7c9712f286aa8b02676cbe27440990d8
gcid <gcid12@gmail.com>
2014-09-27 19:13:16 -0400

    force https

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -1,4 +1,13 @@
 <!DOCTYPE html>
+
+<?php
+if($_SERVER["HTTPS"] != "on")
+{
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    exit();
+}
+?>
+
 <html lang="en">
 <head>
 
